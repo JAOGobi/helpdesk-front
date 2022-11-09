988dfac5d3cb273c9e48e4fee09eb98ae389c9c7
João Gobi <joao_gobi2012@hotmail.com>
2022-11-09 14:35:47 -0300

    rota para componente HOME

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.navigate(['home'])
   }
 
 }
