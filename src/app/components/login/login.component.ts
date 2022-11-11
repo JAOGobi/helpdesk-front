43125d0e1fce9273f3a388fc1b9de2b2b5d0e1b5
João Gobi <joao_gobi2012@hotmail.com>
2022-11-11 14:53:52 -0300

    autenticação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,39 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { Credenciais } from 'src/app/models/credenciais';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { Credenciais } from "src/app/models/credenciais";
+import { ToastrService } from "ngx-toastr";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-
   creds: Credenciais = {
-    email: '',
-    senha: ''
-  }
+    email: "",
+    senha: "",
+  };
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  logar() {
+    this.service.authenticate(this.creds).subscribe(
+      (resposta) => {
+        this.service.successfulLogin(
+          resposta.headers.get("Authorization").substring(7)
+        );
+        this.router.navigate([""]);
+        console.log("aaaa");
+      },
+      () => {
+        this.toast.error("Usuário e/ou senha inválidos!");
+      }
+    );
   }
 
-  logar(){
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha = '';
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid;
   }
-
-  validaCampos(): boolean{
-    if(this.email.valid && this.senha.valid) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
 }
